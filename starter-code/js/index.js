c7162b2dac82f7a607b547dcf1c948959ad6fe99
Nicolas John <nicolas.johnn@gmail.com>
2019-10-12 16:46:32 +0200

    First commit Margot & Nicolas

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,1 +1,45 @@
-// Start here :)
+
+
+
+
+let button = document.querySelector("#calc");
+button.onclick = function(){
+
+    var sum = 0;
+
+    let products = [... document.querySelectorAll(".produit")];
+    products.forEach( function($product){
+
+        
+
+    
+            let prix = +$product.querySelector('.produit .unit_price span').textContent;
+            let input = $product.querySelector('.produit .quantity input').value;
+            let calc = (prix * input).toString();
+            $product.querySelector('.produit .total_price span').innerHTML=calc;
+        
+            
+            sum+=+calc;
+            
+        
+        
+    })
+
+    document.querySelector('.total').innerHTML=sum + "$";
+
+
+}
+
+
+
+let del = [... document.querySelectorAll('.btn-delete')];
+var cart = document.getElementById("cart");
+
+del.forEach( function(el){
+    el.onclick = function(e){
+        let prod = e.currentTarget.parentNode.parentNode;
+        cart.removeChild(prod);
+    }
+})
+
+
