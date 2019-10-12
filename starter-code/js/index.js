f6141d157f33ca52c77ec0f766966f873e06e2d6
Ahlem Khattab <ahlemkhattab@gmail.com>
2019-10-12 16:27:04 +0200

    iteration 2

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,1 +1,22 @@
-// Start here :)
+// Iteration 1
+var btn = document.querySelector ('#calculate-prices');
+
+btn.onclick = function (event) {
+    var tab = [...document.querySelectorAll('.product')];
+    tab.forEach(function(product) {
+
+    var price = Number(product.querySelector('span.unit-price').innerHTML);
+    var quantity = Number(product.querySelector('input.quantity').value);
+    var tot = product.querySelector ('span.total');
+    var tott = price*quantity;
+    tot.innerHTML = tott;
+    return tott;
+
+    })
+
+}
+
+
+
+
+
