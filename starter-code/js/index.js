a6e7f5bb98d866d2f7a3e10380c2dd7b9e8c529f
Sandrine8304 <sandrine.auberval@gmail.com>
2019-10-12 16:32:00 +0200

    on-going cart

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,1 +1,48 @@
 // Start here :)
+
+// document.querySelector(".btn-success").onclick = function() {
+//   console.log("coucou");
+
+//   // recup le pu
+//   var $pu = document.querySelector(".unitPrice");
+//   var pu = Number($pu.innerHTML);
+
+//   // recup qty
+//   var $qty = document.querySelector(".quantity input");
+//   var qty = Number($qty.value);
+//   //console.log($qty);
+
+//   //Prix total
+//   var $subtot = document.querySelector(".tot span");
+//   var subtot = pu * qty;
+
+//   $subtot.innerHTML = subtot;
+//   console.log(subtot);
+// };
+
+//iteration2
+
+document.querySelector(".btn-success").onclick = function() {
+  //console.log("coucou");
+
+  var sum = 0;
+
+  var $products = [...document.querySelectorAll(".product")]; //
+  $products.forEach(function($product) {
+    // recup le pu
+    var $pu = $product.querySelector(".unitPrice");
+    var pu = Number($pu.innerHTML);
+
+    // recup qty
+    var $qty = $product.querySelector(".quantity input");
+    var qty = Number($qty.value);
+    //console.log($qty);
+
+    //Prix total
+    var $subtot = $product.querySelector(".tot span");
+    var subtot = pu * qty;
+
+    $subtot.innerHTML = subtot;
+    console.log(subtot);
+  });
+};
