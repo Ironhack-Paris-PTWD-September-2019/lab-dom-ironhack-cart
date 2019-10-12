3fab75250ae0aef8c044db1d4b523a3b05e8e4a0
Emmanuelle Blancardi <emmanuelleblancardi@MacBook-Pro-de-Emmanuelle.local>
2019-10-12 17:22:01 +0200

    Jenny Emmanuelle

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,1 +1,38 @@
-// Start here :)
+
+var $btnSuccess =document.querySelector(".btn-success");
+
+//var $shop = document.querySelector(".shop");
+
+$btnSuccess.onclick = function calculatePrice (){
+var $products = document.querySelectorAll(".product-row");
+var products = [... $products];
+
+products.forEach(function(product){
+
+  var $price = product.querySelector(".price span");
+  var price = +($price.innerHTML);
+
+  var $quantity = product.querySelector("#input-quantity");
+  var quantity = +($quantity.getAttribute("value"));
+  var totalproduct = price*quantity;
+  var $totalproduct = product.querySelector(".total-product span");
+  $totalproduct.innerHTML = totalproduct;
+});
+
+// on a une erreur mais on ne sait pas pkoi :)
+
+
+//var textTotalprice = document.createElement("h2");
+//textTotalprice.innerHTML = 'Total Price :' + 'totalproduct';
+//$shop.appendChild(textTotalprice);
+//return $totalprice;
+//var textTotalprice = document.createElement("h2");
+//textTotalprice.innerHTML = 'Total Price :' + 'totalprice';
+//$shop.appendChild(textTotalprice);
+//return $totalprice;
+};
+
+
+
+
+
