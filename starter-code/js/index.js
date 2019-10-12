284907ed5ab88b2453d4184ec0d7a9b6ea357ee7
blablamedia <pierre@gambaud.com>
2019-10-12 16:45:51 +0200

    iteration 4: almost done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,1 +1,29 @@
-// Start here :)
+var $calculatePrices = document.getElementById(`calculatePrices`);
+var $numberOfProducts = [...document.querySelectorAll(`.shopping-line`)];
+var totalPrice = 0;
+
+$calculatePrices.onclick = function () {
+    $numberOfProducts.forEach(function(product) {
+        var $unitPrice = product.querySelector(`.unitPrice`);
+        var $unitQuantity = product.querySelector(`.unitQuantity`);
+        var $globalPrice = product.querySelector(`.globalPrice`);
+
+        var productPrice = Number($unitPrice.innerHTML) * Number($unitQuantity.value);
+        totalPrice += productPrice;
+
+        return $globalPrice.innerHTML = productPrice;
+    });
+
+    var $finalPrice = document.getElementById(`finalPrice`);
+    $finalPrice.innerHTML = totalPrice;
+}
+
+// *****************
+
+var $deleteButtons = [...document.querySelectorAll(`.btn-delete`)];
+
+$deleteButtons.forEach(function($deleteButton) {
+    $deleteButton.onclick = function () {
+        console.log(`coucou`)
+    }
+})
