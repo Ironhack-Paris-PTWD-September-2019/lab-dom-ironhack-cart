5537954fdccaf2e9343d76a0c4eb3051ba622876
FannyFerg <53188359+FannyFerg@users.noreply.github.com>
2019-10-12 17:32:51 +0200

    Lab Sophie Fanny

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,1 +1,34 @@
 // Start here :)
+
+let button = document.getElementsByClassName("totalb")[0];
+
+button.onclick = function() {
+  var somme = 0;
+
+  //recuperer la quantité
+  var $product = [...document.querySelectorAll(".product")];
+  $product.forEach(function($product) {
+    let $quantite = $product.querySelector(".qty input");
+    let quantite = Number($quantite.value);
+    // recuperer le prix
+    let $price = $product.querySelector(".price span");
+    let price = Number($price.innerHTML);
+    let total = price * quantite;
+    let $total = $product.querySelector(".total span");
+    $total.innerHTML = total;
+
+    somme += total;
+  });
+
+  let $totalprice = document.querySelector(".totalprice");
+  $totalprice.innerHTML =somme;
+
+};
+let buttonDel = document.getElementsByClassName("btn btn-delete")[0];
+buttonDel.onclick = function(event){
+  console.log('buttonDel');
+  let enfant=event.currentTarget.parentNode.parentNode; console.log(enfant);
+  let $products = document.getElementsByClassName("products")[0]; console.log($products);
+  $products.removeChild(enfant);
+
+}
