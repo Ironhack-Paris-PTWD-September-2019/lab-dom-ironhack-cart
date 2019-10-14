b8d6233508287a48943ca54ad3f7729bb3175e96
blablamedia <pierre@gambaud.com>
2019-10-14 23:17:15 +0200

    iteration 5: done!

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,29 +1,90 @@
-var $calculatePrices = document.getElementById(`calculatePrices`);
-var $numberOfProducts = [...document.querySelectorAll(`.shopping-line`)];
-var totalPrice = 0;
+// iteration 3: Iteration 3: Calculating the total price of all products
+function renderPrice() { 
+    var $calculatePrices = document.getElementById(`calculatePrices`);
+    
+    $calculatePrices.onclick = function () {
+        var $numberOfProducts = [...document.querySelectorAll(`.shopping-line`)];
+        var totalPrice = 0;
 
-$calculatePrices.onclick = function () {
-    $numberOfProducts.forEach(function(product) {
-        var $unitPrice = product.querySelector(`.unitPrice`);
-        var $unitQuantity = product.querySelector(`.unitQuantity`);
-        var $globalPrice = product.querySelector(`.globalPrice`);
+        $numberOfProducts.forEach(function(product) {
+            var $unitPrice = product.querySelector(`.unitPrice`);
+            var $unitQuantity = product.querySelector(`.unitQuantity`);
+            var $globalPrice = product.querySelector(`.globalPrice`);
 
-        var productPrice = Number($unitPrice.innerHTML) * Number($unitQuantity.value);
-        totalPrice += productPrice;
+            var productPrice = Number($unitPrice.innerHTML) * Number($unitQuantity.value);
+            totalPrice += productPrice;
 
-        return $globalPrice.innerHTML = productPrice;
-    });
+            return $globalPrice.innerHTML = productPrice.toFixed(2);
+        });
 
-    var $finalPrice = document.getElementById(`finalPrice`);
-    $finalPrice.innerHTML = totalPrice;
+        var $finalPrice = document.getElementById(`finalPrice`);
+        $finalPrice.innerHTML = totalPrice.toFixed(2);
+    }
 }
 
-// *****************
+// iteration 4: Deleting a product
+function renderDeleting() { 
+    var $shoppingItems = [...document.querySelectorAll(`.shopping-line`)];
 
-var $deleteButtons = [...document.querySelectorAll(`.btn-delete`)];
+    $shoppingItems.forEach(function($shoppingItem) {
+        var $deleteButton = $shoppingItem.querySelector(`.btn-delete`);
 
-$deleteButtons.forEach(function($deleteButton) {
-    $deleteButton.onclick = function () {
-        console.log(`coucou`)
+        $deleteButton.onclick = function () {
+            $shoppingItem.parentNode.removeChild($shoppingItem);
+        }
+    })
+}
+
+// iteration 5: Creating new products
+var $newProductCreateButton = document.getElementById(`newProductCreateButton`);
+
+$newProductCreateButton.onclick = function (){
+    var $newProductName = document.getElementById(`newProductName`);
+    var $newProductPrice = document.getElementById(`newProductPrice`);
+    
+    var newProductLine = document.createElement('div');
+    newProductLine.innerHTML =
+        `<div class="shopping-line">
+            <div class="productName">
+                <span></span>
+            </div>
+            <div class="productPrice">
+                $<span class="unitPrice"></span>
+            </div>
+            <div>
+                <label>
+                    QTY <input type="number" value="0" min="0" class="unitQuantity">
+                </label>
+            </div>
+            <div class="productGlobalPrice">
+                $<span class="globalPrice">0.00</span>
+            </div>
+            <div>
+                <button class="btn btn-delete">delete</button>
+            </div>
+        </div>`;
+
+    if($newProductName.value !== `` & $newProductPrice.value !== `` & !isNaN($newProductPrice.value)) {
+        newProductLine.querySelector(`.productName span`).innerHTML = $newProductName.value;
+        newProductLine.querySelector(`.unitPrice`).innerHTML = Number($newProductPrice.value).toFixed(2);
+
+        var $shoppingList = document.getElementById(`shopping-list`);
+        $shoppingList.appendChild(newProductLine);
+
+        $newProductName.value = '';
+        $newProductPrice.value = '';
+        $newProductName.style.borderColor = `black`;
+        $newProductPrice.style.borderColor = `black`;
+
+        renderPrice();
+        renderDeleting();
+    } else {
+        $newProductName.style.borderColor = `red`;
+        $newProductPrice.style.borderColor = `red`;
     }
-})
+}
+
+renderPrice();
+renderDeleting();
+
+
